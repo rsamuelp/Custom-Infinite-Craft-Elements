5f8623b0d6369de08242750a4b95876d89e8766b
rsamuelp <realsamuelp2@gmail.com>
2024-10-07 17:28:34 -0500

    Create Narrow-One.js

---
+++ Narrow-One.js
@@ -0,0 +1,97 @@
+const Ot = new Map;
+Ot.set("movementSpeed", {
+    icon: "static/img/menuUI/shop/classIcons/movementSpeed.svg",
+    uiName: "Movement Speed",
+    defaultValue: 10,
+    min: 0,
+    max: 20,
+    tooltipCategories: ["armor"]
+}),
+Ot.set("armorStrength", {
+    icon: "static/img/menuUI/shop/classIcons/armorStrength.svg",
+    uiName: "Damage Protection",
+    defaultValue: 10,
+    min: 0,
+    max: 20,
+    tooltipCategories: ["armor"]
+}),
+Ot.set("healthRegenSpeed", {
+    icon: "static/img/menuUI/shop/classIcons/healthRegenSpeed.svg",
+    uiName: "Health Regen Speed",
+    defaultValue: 10,
+    min: 0,
+    max: 20,
+    tooltipCategories: ["armor"]
+}),
+Ot.set("shootingFocus", {
+    icon: "static/img/menuUI/shop/classIcons/shootingFocus.svg",
+    uiName: "Focus",
+    defaultValue: 10,
+    min: 0,
+    max: 10,
+    tooltipCategories: ["bow"]
+}),
+Ot.set("bowAttackStrength", {
+    icon: "static/img/menuUI/shop/classIcons/bowAttackStrength.svg",
+    uiName: "Attack Strength",
+    defaultValue: 10,
+    min: 0,
+    max: 10,
+    tooltipCategories: ["bow", "arrow"]
+}),
+Ot.set("arrowLoadingSpeed", {
+    icon: "static/img/menuUI/shop/classIcons/arrowLoadingSpeed.svg",
+    uiName: "Loading Speed",
+    defaultValue: 5,
+    min: 0,
+    max: 5,
+    tooltipCategories: ["bow"]
+}),
+Ot.set("arrowFlySpeed", {
+    icon: "static/img/menuUI/shop/classIcons/arrowFlySpeed.svg",
+    uiName: "Travel Speed",
+    defaultValue: 5,
+    min: 0,
+    max: 5,
+    tooltipCategories: ["arrow"]
+}),
+Ot.set("arrowEnemyStun", {
+    icon: "static/img/menuUI/shop/classIcons/arrowEnemyStun.svg",
+    uiName: "Stun Enemy",
+    defaultValue: 5,
+    min: 0,
+    max: 5,
+    tooltipCategories: ["arrow"]
+}),
+Ot.set("meleeAttackStrength", {
+    icon: "static/img/menuUI/shop/classIcons/meleeAttackStrength.svg",
+    uiName: "Strength",
+    defaultValue: 5,
+    min: 0,
+    max: 5,
+    tooltipCategories: ["melee"]
+}),
+Ot.set("meleeAttackSpeed", {
+    icon: "static/img/menuUI/shop/classIcons/meleeAttackSpeed.svg",
+    uiName: "Speed",
+    defaultValue: 5,
+    min: 0,
+    max: 5,
+    tooltipCategories: ["melee"]
+}),
+Ot.set("meleeAttackReach", {
+    icon: "static/img/menuUI/shop/classIcons/meleeAttackReach.svg",
+    uiName: "Reach",
+    defaultValue: 5,
+    min: 0,
+    max: 5,
+    tooltipCategories: ["melee"]
+}),
+Ot.set("bloodlust", {
+    icon: "static/img/menuUI/shop/classIcons/bloodlust.svg",
+    uiName: "Bloodlust",
+    defaultValue: 11,
+    min: 0,
+    max: 11,
+    tooltipCategories: ["armor", "bow", "arrow", "melee"]
+});
